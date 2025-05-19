e706d6c5688ce6a73d01e0ae67af59304c33ee41
Micunymos2000 <149001337+micunymos@users.noreply.github.com>
2025-05-20 00:23:20 +0200

    Added a demo disk.
    Why not? Check the boot manager, everything. Just more function to it off the shelf.

---
+++ hdd/osloader.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d %~dp0
+echo This boot mode is not supported. Check osinf.txt!
+pause >nul
---
+++ hdd/start.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d %~dp0
+echo This boot mode is not supported currently. Make it be! :-^)
+pause >nul
